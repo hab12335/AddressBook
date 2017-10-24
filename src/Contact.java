f9dd8b340c4ddf081faf3fcb6fffe429b74dbc9b
Hugo Botas <hab12335@campus.fct.unl.pt>
2017-10-24 21:51:31 +0100

    Class contact feita

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,39 @@
 public class Contact {
+    private String name, email;
+    private int phone;
+
+
+    public Contact(String name, int phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public boolean equals(Contact otherContact) {
+        return this.name.equals(otherContact.getName());
+    }
+
+    public int compareTo(Contact otherContact) {
+        return this.name.compareTo(otherContact.getName());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String option;
+        Scanner in = new Scanner(System.in);
+        do {
+            option = readCommand(in);
+            if (!option.equals("Q")) {
+                switch (option) {
+                    case "AC":
+                        break;
+                    case "RC":
+                        break;
+                    case "GP":
+                        break;
+                    case "GE":
+                        break;
+                    case "SP":
+                        break;
+                    case "SE":
+                        break;
+                    case "LC":
+                        break;
+                    default:
+                        System.out.println("Comando Desconhecido");
+                }
+            }
+
+        } while (!option.equals("Q"));
+        in.close();
+    }
+
+    private static String readCommand(Scanner in) {
+        String option;
+        System.out.println("> AC (adiciona um contacto)");
+        System.out.println("> RC (remove um contacto)");
+        System.out.println("> GP (consulta o telefone de um contacto)");
+        System.out.println("> GE (consulta o e-mail de um contacto)");
+        System.out.println("> SP (actualiza o telefone de um dado contacto)");
+        System.out.println("> SE (actualiza o e-mail de um dado contacto)");
+        System.out.println("> LC (lista todos os contactos existentes na agenda)");
+        System.out.println("> Q (sair)");
+        option = in.nextLine().toUpperCase();
+        return option;
     }
 }
